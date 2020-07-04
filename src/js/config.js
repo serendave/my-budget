f00bbe9e860ff2fab70a63bd6040d505cf2def76
Davis <hey@gmail.com>
2020-07-04 17:49:14 +0300

    changed http request

--- src/js/config.js
+++ src/js/config.js
@@ -5,7 +5,7 @@ const key = "44ba7104aa50ae794b60dcd4e722ab66";
 
 export async function getCurrencies() {
     try {
-        const result = await axios(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,UAH`);
+        const result = await axios(`https://data.fixer.io/api/latest?access_key=${key}&symbols=USD,UAH`);
         return {
             dollarPerEuro: parseFloat((result.data.rates.USD).toFixed(2)),
             uahPerEuro: parseFloat((result.data.rates.UAH).toFixed(2)),
